25e33a759a6b7ac09b4feafb67507c81d66a9150
Computer Inside <trystanlandino0@gmail.com>
2017-11-20 16:08:57 -0500

    Changed the UI to a tab based system. We thought this would become cleaner as we added features and code.

--- HexDeploy/Form1.Designer.cs
+++ HexDeploy/Form1.Designer.cs
@@ -30,10 +30,6 @@
         {
             this.sendingTextbox = new System.Windows.Forms.TextBox();
             this.sendbutton = new System.Windows.Forms.Button();
-            this.mainmenuStrip = new System.Windows.Forms.MenuStrip();
-            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.connectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label4 = new System.Windows.Forms.Label();
             this.testbutton = new System.Windows.Forms.Button();
             this.handleTextbox = new System.Windows.Forms.TextBox();
@@ -44,64 +40,39 @@
             this.label1 = new System.Windows.Forms.Label();
             this.IPtextbox = new System.Windows.Forms.TextBox();
             this.msgTextbox = new System.Windows.Forms.RichTextBox();
-            this.mainmenuStrip.SuspendLayout();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // sendingTextbox
             // 
             this.sendingTextbox.AcceptsTab = true;
-            this.sendingTextbox.Location = new System.Drawing.Point(12, 426);
+            this.sendingTextbox.Location = new System.Drawing.Point(8, 400);
             this.sendingTextbox.Name = "sendingTextbox";
-            this.sendingTextbox.Size = new System.Drawing.Size(537, 20);
+            this.sendingTextbox.Size = new System.Drawing.Size(538, 20);
             this.sendingTextbox.TabIndex = 2;
             this.sendingTextbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.sendingTextbox_KeyDown);
             this.sendingTextbox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.sendingTextbox_KeyUp);
             // 
             // sendbutton
             // 
-            this.sendbutton.Location = new System.Drawing.Point(564, 424);
+            this.sendbutton.Location = new System.Drawing.Point(562, 392);
             this.sendbutton.Name = "sendbutton";
-            this.sendbutton.Size = new System.Drawing.Size(75, 23);
+            this.sendbutton.Size = new System.Drawing.Size(75, 35);
             this.sendbutton.TabIndex = 3;
             this.sendbutton.Text = "Send";
             this.sendbutton.UseVisualStyleBackColor = true;
             this.sendbutton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.sendbutton_MouseClick);
             // 
-            // mainmenuStrip
-            // 
-            this.mainmenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.mainmenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.settingsToolStripMenuItem,
-            this.connectionToolStripMenuItem,
-            this.infoToolStripMenuItem});
-            this.mainmenuStrip.Location = new System.Drawing.Point(0, 0);
-            this.mainmenuStrip.Name = "mainmenuStrip";
-            this.mainmenuStrip.Size = new System.Drawing.Size(651, 24);
-            this.mainmenuStrip.TabIndex = 16;
-            this.mainmenuStrip.Text = "menuStrip1";
-            // 
-            // settingsToolStripMenuItem
-            // 
-            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.settingsToolStripMenuItem.Text = "Settings";
-            // 
-            // connectionToolStripMenuItem
-            // 
-            this.connectionToolStripMenuItem.Name = "connectionToolStripMenuItem";
-            this.connectionToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
-            this.connectionToolStripMenuItem.Text = "Connection";
-            // 
-            // infoToolStripMenuItem
-            // 
-            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
-            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
-            this.infoToolStripMenuItem.Text = "Info";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(444, 23);
+            this.label4.Location = new System.Drawing.Point(136, 51);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 13);
             this.label4.TabIndex = 14;
@@ -109,17 +80,17 @@
             // 
             // testbutton
             // 
-            this.testbutton.Location = new System.Drawing.Point(564, 35);
+            this.testbutton.Location = new System.Drawing.Point(331, 46);
             this.testbutton.Name = "testbutton";
-            this.testbutton.Size = new System.Drawing.Size(73, 23);
+            this.testbutton.Size = new System.Drawing.Size(136, 23);
             this.testbutton.TabIndex = 15;
-            this.testbutton.Text = "Test";
+            this.testbutton.Text = "Test Connection";
             this.testbutton.UseVisualStyleBackColor = true;
             this.testbutton.Click += new System.EventHandler(this.button5_Click);
             // 
             // handleTextbox
             // 
-            this.handleTextbox.Location = new System.Drawing.Point(447, 38);
+            this.handleTextbox.Location = new System.Drawing.Point(183, 48);
             this.handleTextbox.Name = "handleTextbox";
             this.handleTextbox.Size = new System.Drawing.Size(102, 20);
             this.handleTextbox.TabIndex = 13;
@@ -127,7 +98,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(361, 23);
+            this.label3.Location = new System.Drawing.Point(8, 51);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(26, 13);
             this.label3.TabIndex = 12;
@@ -135,7 +106,7 @@
             // 
             // PortTextbox
             // 
-            this.PortTextbox.Location = new System.Drawing.Point(364, 38);
+            this.PortTextbox.Location = new System.Drawing.Point(40, 48);
             this.PortTextbox.Name = "PortTextbox";
             this.PortTextbox.Size = new System.Drawing.Size(77, 20);
             this.PortTextbox.TabIndex = 7;
@@ -143,7 +114,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(228, 23);
+            this.label2.Location = new System.Drawing.Point(314, 13);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 11;
@@ -151,7 +122,7 @@
             // 
             // passwdtextbox
             // 
-            this.passwdtextbox.Location = new System.Drawing.Point(231, 39);
+            this.passwdtextbox.Location = new System.Drawing.Point(373, 10);
             this.passwdtextbox.Name = "passwdtextbox";
             this.passwdtextbox.Size = new System.Drawing.Size(121, 20);
             this.passwdtextbox.TabIndex = 8;
@@ -159,7 +130,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Location = new System.Drawing.Point(8, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 10;
@@ -167,18 +138,72 @@
             // 
             // IPtextbox
             // 
-            this.IPtextbox.Location = new System.Drawing.Point(12, 39);
+            this.IPtextbox.Location = new System.Drawing.Point(72, 10);
             this.IPtextbox.Name = "IPtextbox";
             this.IPtextbox.Size = new System.Drawing.Size(213, 20);
             this.IPtextbox.TabIndex = 9;
             // 
             // msgTextbox
             // 
-            this.msgTextbox.Location = new System.Drawing.Point(12, 63);
+            this.msgTextbox.Location = new System.Drawing.Point(3, 6);
             this.msgTextbox.Name = "msgTextbox";
-            this.msgTextbox.Size = new System.Drawing.Size(625, 353);
+            this.msgTextbox.Size = new System.Drawing.Size(632, 383);
             this.msgTextbox.TabIndex = 17;
             this.msgTextbox.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(651, 456);
+            this.tabControl1.TabIndex = 18;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage1.Controls.Add(this.msgTextbox);
+            this.tabPage1.Controls.Add(this.sendbutton);
+            this.tabPage1.Controls.Add(this.sendingTextbox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(643, 430);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Chat";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.IPtextbox);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.testbutton);
+            this.tabPage2.Controls.Add(this.passwdtextbox);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.handleTextbox);
+            this.tabPage2.Controls.Add(this.PortTextbox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(643, 430);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Connection";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(643, 430);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Settings";
+            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // mainform
             // 
@@ -186,38 +211,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(651, 456);
-            this.Controls.Add(this.msgTextbox);
-            this.Controls.Add(this.testbutton);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.handleTextbox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.IPtextbox);
-            this.Controls.Add(this.passwdtextbox);
-            this.Controls.Add(this.PortTextbox);
-            this.Controls.Add(this.sendbutton);
-            this.Controls.Add(this.sendingTextbox);
-            this.Controls.Add(this.mainmenuStrip);
+            this.Controls.Add(this.tabControl1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "mainform";
             this.Text = "2017-11-19";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.mainmenuStrip.ResumeLayout(false);
-            this.mainmenuStrip.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button sendbutton;
-        private System.Windows.Forms.MenuStrip mainmenuStrip;
-        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem connectionToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button testbutton;
         private System.Windows.Forms.TextBox handleTextbox;
@@ -229,6 +240,10 @@
         private System.Windows.Forms.TextBox IPtextbox;
         public System.Windows.Forms.TextBox sendingTextbox;
         private System.Windows.Forms.RichTextBox msgTextbox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
 
