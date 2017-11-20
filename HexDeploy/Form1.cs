2c10082c21b20c49dcc563e2f23a9da508b575e7
tland <trystanlandino0@gmail.com>
2017-11-19 19:08:28 -0500

    oh for fuck's sake

---
+++ HexDeploy/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Net;
+using System.Text;
+
+namespace HexDeploy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Thread thread0 = new Thread(packettest);
+            thread0.Start();
+        }
+        private void packettest()
+        {
+            Socket test = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            try
+            {
+                IPAddress serverAddr = IPAddress.Parse(textBox5.Text);
+                try
+                {
+                    int new0 = Convert.ToInt32(textBox3.Text);
+                    IPEndPoint ep0 = new IPEndPoint(serverAddr, new0);
+                    string text = "testing!";
+                    byte[] buffer0 = Encoding.ASCII.GetBytes(text );
+                    test.SendTo(buffer0, ep0);
+                }
+                catch
+                {
+                    MessageBox.Show("Please input a valid port number!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please input a valid IP address!");
+            }
+        }
+    }
+}
