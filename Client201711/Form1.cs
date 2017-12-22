2742d30104797699fd3beef95459fcca3944c441
Computer Inside <trystanlandino0@gmail.com>
2017-12-22 01:38:45 -0500

    Add even more fucking comments.

--- Client201711/Form1.cs
+++ Client201711/Form1.cs
@@ -11,44 +11,44 @@ namespace HexDeploy
     
     public partial class mainform : Form
     {
+        //Connection Thread
         Thread connectThread;
+        //TCP socket
         TcpClient clsock;
         NetworkStream servStream;
         string rd;
-
+        //fixes spam problems with holding down the enter key
         public bool keydown = false;
+        //Username/alias of the user
         public string handlenm = string.Empty;
+        //Deprecated/not implemented
         public string password;
 
-        public static string Base64Encode(string plain)
-        {
-            var plainbyte = System.Text.Encoding.ASCII.GetBytes(plain);
-            return System.Convert.ToBase64String(plainbyte);
-        }
-     
+        //Useless function. May be removed near future.
+        //public static string Base64Encode(string plain)
+        //{
+        //    var plainbyte = System.Text.Encoding.ASCII.GetBytes(plain);
+        //    return System.Convert.ToBase64String(plainbyte);
+        //}
+
         public mainform()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-           
         }
         private void button3_MouseClick(object sender, EventArgs e)
         {
-           
             byte[] outS = Encoding.ASCII.GetBytes(sendingTextbox.Text + "$");
             servStream.Write(outS, 0, outS.Length);
-            //msgTextbox.AppendText(GetTimeStamp(DateTime.UtcNow) + "Me:" + sendingTextbox.Text + "\n");
             sendingTextbox.Text = "";
             servStream.Flush();
             if (!sendingTextbox.Focused)
             {
+                //attempting to refocus to the sending message textbox
                 sendingTextbox.Focus();
             }
-
-
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -64,6 +64,7 @@ namespace HexDeploy
 
             try
             {
+                //try to parse the string in the IPtextbox
                 IPAddress serverAddr = IPAddress.Parse(IPtextbox.Text);
                 try
                 {
@@ -99,12 +100,8 @@ namespace HexDeploy
             {
                 keydown = true;
                 button3_MouseClick(this, new EventArgs());
-
                 e.Handled = true;
-
                 e.SuppressKeyPress = true;
-                //msgTextbox.AppendText("\n" + sendingTextbox.Text + "\n" + GetTimeStamp(DateTime.UtcNow));
-                //sendbutton.KeyDown -= new KeyEventHandler(sendbutton_KeyDown);
             }
         }
         private void sendingTextbox_KeyUp(object sender, KeyEventArgs e)
@@ -116,26 +113,22 @@ namespace HexDeploy
         }
         private void sendbutton_MouseClick(object sender, MouseEventArgs e)
         {
+
             byte[] outS = Encoding.ASCII.GetBytes(sendingTextbox.Text + "$");
             servStream.Write(outS, 0, outS.Length);
-            //msgTextbox.AppendText(GetTimeStamp(DateTime.UtcNow) + "Me:" + sendingTextbox.Text + "\n");
             sendingTextbox.Text = "";
             servStream.Flush();
             
         }
-
         private void HndlButton_Click(object sender, EventArgs e)
         {
+            //setting the users' alias
             handlenm = Convert.ToString(handleTextbox.Text);
+            //enabling the connect button once username is set.
             connectButton.Enabled = true;
         }
         private void passconnect()
         {
-            //password = Convert.ToString(passwdtextbox.Text);
-
-            //password = Base64Encode(password);
-            //MessageBox.Show(password);
-
         }
 
         private void connectButton_Click(object sender, EventArgs e)
@@ -147,11 +140,14 @@ namespace HexDeploy
             string ip = "";
             byte[] outS = null;
             int port = 0;
+            //invoke the netstream
             servStream = default(NetworkStream);
+            //invoke tcpclient
             clsock = new TcpClient();
 
             try
             {
+               
                 ip = Convert.ToString(IPtextbox.Text);
                 port = Convert.ToInt32(PortTextbox.Text);
                 try
@@ -170,8 +166,6 @@ namespace HexDeploy
             }
 
             connectThread = new Thread(getMsg);
-            //clsock.Connect(ip, port);
-            //servStream = clsock.GetStream(); 
             try
             {
                 outS = Encoding.ASCII.GetBytes(handlenm + "$");
@@ -216,7 +210,6 @@ namespace HexDeploy
                     }
                     catch
                     {
-
                     }
                     messaging();
                 }
@@ -237,7 +230,6 @@ namespace HexDeploy
                 }
                 catch
                 {
-                      
                 }
                 
             }
@@ -249,7 +241,6 @@ namespace HexDeploy
                 }
                 catch
                 {
-
                 }
                 
             }
@@ -261,11 +252,7 @@ namespace HexDeploy
             {
                 connectButton.Enabled = true;
                 LingerOption lingerOption = new LingerOption(true, 0);
-
-
-                
                 Disconnect();
-
             }
         }
         private void Disconnect()
@@ -276,10 +263,7 @@ namespace HexDeploy
             servStream.Flush();
             clsock.Close();
             servStream.Close();
-            //servStream.Dispose();
             connectThread.Abort();
-
-
         }
     }
 }
